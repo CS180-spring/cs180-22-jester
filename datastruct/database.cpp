9036395dda48afed1d0c3d57aecd46193639a06c
edgar2020 <edgar2020@chromium.org>
2023-04-19 21:39:44 -0700

    commit small changes to recieve changes in bramch

--- datastruct/database.cpp
+++ datastruct/database.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void Database::createTable(string tableName, int colNums, &vector<vector<string>> columnNames)
 {
     table* t = new table(tableName, colNums, columnNames);
+    db_map.insert({tableName, t});
 
 }//will create new datatable (end of createTable)
         
