7904a713319e32d1fcc17e5aa4c61e5f49754184
edgar2020 <edgar2020@chromium.org>
2023-04-19 20:45:56 -0700

    added constructor and destructor to Database

--- datastruct/database.h
+++ datastruct/database.h
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -11,6 +12,26 @@ class Database
     private:
         string db_name ="";
         map<string, table*> db_map;
+
+    public:
+        database(string db_name) : db_name(db_name);
+        ~database()
+        {
+            for(map<string, table*>::iterator itr = db_map.begin(); itr != db_map.end(); itr++)
+            {
+                delete (itr->second);
+            }
+            textureMap.clear();
+        }
+        void createTable();//will create new datatable
+        void deleteTable();//will delete table from database
+        vector<vector<string>> printRAW();//will print raw of all tables in database
+        vector<vector<string>> printRAW(string);//will print raw of a single table
+        vector<vector<string>> createQuery(/*Some way to read queries*/);
+        void createTableFromCSVFile(/**HOwever we read in a file*/);
+        
+
+    
 
 
 
