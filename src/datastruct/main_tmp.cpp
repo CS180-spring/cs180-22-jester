d45488c789cc6f1941ad454a1a07bd9d76b0ba77
Edgarventura Melendrez <emele012@bolt.cs.ucr.edu>
2023-05-16 23:50:19 -0700

    formating changes

--- src/datastruct/main_tmp.cpp
+++ src/datastruct/main_tmp.cpp
@@ -10,19 +10,19 @@ int main()
 {
     Database* db = new Database("mydb");
 
-    // vector<string> test1 {"name","age","desc"};
-    // db->createTable("Teammate", 3, test1);
+    vector<string> test1 {"name","age","desc"};
+    db->createTable("Teammate", 3, test1);
 
-    // vector<string> test2 {"edgar","24","test"};
-    // vector<string> test3 {"luis","21","test"};
-    // vector<string> test4 {"owen","23","test"};
+    vector<string> test2 {"Edgar","24","test"};
+    vector<string> test3 {"Luis","21","test"};
+    vector<string> test4 {"Owen","23","test"};
     // // // vector<string> test {"owen","23","test"};
     // vector<string> test5 {"owen","23"};
     // vector<string> test6 {"owen","23", "", ""};
     
-    // db->getTable("Teammate")->add_row(test2);
-    // db->getTable("Teammate")->add_row(test3);
-    // db->getTable("Teammate")->add_row(test4);
+    db->getTable("Teammate")->add_row(test2);
+    db->getTable("Teammate")->add_row(test3);
+    db->getTable("Teammate")->add_row(test4);
 
     // db->getTable("Teammate")->add_row(test5);
     // db->getTable("Teammate")->add_row(test6);
@@ -40,20 +40,20 @@ int main()
     // Table* temp = db->getTable("people");
     // db->createView(temp->g_num_of_cols(), temp->g_name_of_cols(), temp->g_all_data());
     // cout<<"sdfs";
-    DataView* dv = db->createView({"people", "orgs"});
+    DataView* dv = db->createView({"Teammate", "people"});
     // DataView* dv = db->createView(db->getTable("people"));
     // cout<<"sdfs";
     // dv->filter(".Sex", "Female", true);
-    // dv->filter(".First Name", "Eddie", true);
+    dv->filter(".name", ".First Name", true);
     // dv->filter(".First Namdfe", "Eddie", false);
     // dv->range(".Index", "1", "10", false);
     // dv->print_all_data();
-    dv->delete_column(".Index");
-    dv->delete_column(".User Id");
+    dv->delete_column(".name");
+    dv->delete_column(".desc");
     dv->delete_column(".Date of birth");
-    dv->delete_column(".Website");
-    dv->delete_column(".Description");
-    dv->delete_column(".Name");
+    dv->delete_column(".age");
+    dv->delete_column(".User Id");
+    // dv->delete_column(".Name");
     dv->print_all_data();
     // dv->range();
     // cout<<"sdfs";
