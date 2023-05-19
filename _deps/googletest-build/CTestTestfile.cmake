646dc696f1a9c7749fa6d9e0186c27553756c759
nuharahman9 <nrahm009@ucr.edu>
2023-05-18 23:00:57 -0700

    Revert "Revert "bye branch""
    This reverts commit 5f35e168568322d90d7a4ff1eff52d4b1dc769db.

---
+++ _deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/nuharahman/Desktop/cs180-22-jester-1/_deps/googletest-src
+# Build directory: /Users/nuharahman/Desktop/cs180-22-jester-1/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ tabletest[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/Users/nuharahman/Desktop/cs180-22-jester-1/tabletest[1]_tests.cmake")
+  include("/Users/nuharahman/Desktop/cs180-22-jester-1/tabletest[1]_tests.cmake")
+else()
+  add_test(tabletest_NOT_BUILT tabletest_NOT_BUILT)
+endif()
