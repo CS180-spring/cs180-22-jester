02e025be079e126cb77ba6c59f112de79932f1ac
Edgar Melendrez <evmelendrez@gmail.com>
2023-05-27 12:40:12 -0700

    refactoring schema

--- datastruct/schema.cpp
+++ datastruct/schema.cpp
@@ -1,5 +1,41 @@
 #include "schema.h"
 
+Schema::Schema()
+{
+    table = {}; 
+    name_of_colums = {}; 
+    num_of_cols = 0;
+    num_of_rows = 0;
+} // end of 0 arg schema constructor
+
+Schema::Schema(int num_of_cols)
+{
+    this->num_of_cols = num_of_cols;
+
+    cout << "enter the labels for the table" ; 
+    for(unsigned int i = 0; i < num_of_cols;++i){
+        string s; 
+        cin >> s;
+        name_of_colums.push_back(s);  
+    }
+}//end of 1 arg schema constructor
+
+Schema::Schema(int num_of_cols, vector<string> &columnNames)
+{
+    name_of_colums = columnNames;
+    this->num_of_cols = num_of_cols;
+}
+
+Schema::Schema(int num_of_cols, vector<string> &columnNames, vector<vector<string> > t )
+{
+    // cout<<"test 1"<<endl;
+    name_of_colums = columnNames;
+    // cout<<"test 2"<<endl;
+    table = t;
+    // cout<<"test 3"<<endl;
+    num_of_cols = num_of_cols;
+}
+
 vector<string> Schema::g_name_of_cols(){
     return name_of_colums;
 }
@@ -35,15 +71,15 @@ bool Schema::does_this_col_name_exist(string s){
 
 
 void Schema::print_col_names(){
-    for(int i = 0; i < name_of_colums.size(); ++i){
+    for(unsigned int i = 0; i < name_of_colums.size(); ++i){
         cout << name_of_colums.at(i)<<"\t";
     }
 }
 
 void Schema::print_all_data(){
     // cout<<"### Data for "<< this->name_of_table <<"##"<<endl;
-    int i = 0;
-    int j = 0;
+    unsigned int i = 0;
+    unsigned int j = 0;
     // cout<<"\033[1;34mTable Name: "<<table
 
     for(j = 0 ; j < name_of_colums.size()-1; ++j){
@@ -67,13 +103,13 @@ void Schema::print_all_data(){
 
 int Schema::columnExisits(string column)
 {
-    int i = 0;
+    unsigned int i = 0;
     if(column.at(0)=='.')//remove dot for column's name
     {
         column = column.substr(1, column.size());
         // cout<<"||"<<column<<endl;
     }
-    for(int i = 0; i < name_of_colums.size(); i++)
+    for(i = 0; i < name_of_colums.size(); i++)
     {
         if(column.compare(name_of_colums.at(i)) == 0)
         {
@@ -155,7 +191,7 @@ void Schema::delete_column(string s){
     name_of_colums.erase(it);
     num_of_cols = name_of_colums.size();
     // cout<<table.size()<<endl;
-    for(int i = 0 ; i < table.size();++i){
+    for(unsigned int i = 0 ; i < table.size();++i){
         // cout<<i<<"\t";
         table.at(i).erase(table.at(i).begin()+loc);
     }
--- datastruct/schema.h
+++ datastruct/schema.h
@@ -13,38 +13,10 @@ using namespace std;
 class Schema{
 
     public: 
-    Schema(){
-        table = {}; 
-        name_of_colums = {}; 
-        num_of_cols = 0;
-        num_of_rows = 0;
-    }
-
-    Schema(int num_of_cols){;
-        this->num_of_cols = num_of_cols;
-
-        cout << "enter the labels for the table" ; 
-        for(int i = 0; i < num_of_cols;++i){
-            string s; 
-            cin >> s;
-            name_of_colums.push_back(s);  
-        }
-    }
-
-    Schema(int num_of_cols, vector<string> &columnNames){
-        name_of_colums = columnNames;
-        this->num_of_cols = num_of_cols;
-    }
-
-    Schema(int num_of_cols, vector<string> &columnNames, vector<vector<string> > t ){
-        // cout<<"test 1"<<endl;
-        name_of_colums = columnNames;
-        // cout<<"test 2"<<endl;
-        table = t;
-        // cout<<"test 3"<<endl;
-        num_of_cols = num_of_cols;
-    }
-
+    Schema();
+    Schema(int );
+    Schema(int, vector<string> &);
+    Schema(int, vector<string> &, vector<vector<string> > );
 
     // CRUD
     // CREATE
@@ -73,10 +45,10 @@ class Schema{
     protected:
     vector<vector<string> > table; 
     vector<string> name_of_colums; 
-    int num_of_rows;
-    int num_of_cols;
+    unsigned int num_of_rows;
+    unsigned int num_of_cols;
 
-}; 
+};
 
 
 #endif
